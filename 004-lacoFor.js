8dd681ff9b375c1636569f55aa9df9fb254e5e46
Pablo Ramon <ramonparaiba@gmail.com>
2020-10-14 12:42:41 +0000

    Laço For e array

---
+++ 004-lacoFor.js
@@ -0,0 +1,9 @@
+let incomes = [6500, 6700, 7200];
+
+for (let income of incomes){
+    income += 5000;
+    console.log(income);    
+}
+
+console.log(incomes);
+//Pq o valor final não é incrementado?
