b0aac1254172a9ca9577ce24170c085f260a4691
Pablo Ramon <ramonparaiba@gmail.com>
2020-10-16 20:37:44 +0000

    Arrow Functions

---
+++ 007-ArrowFunctions.js
@@ -0,0 +1,32 @@
+// let ola = function(){ 
+//     return 'Hello Chimpanzé'; 
+// }
+// console.log(ola());
+
+// let ola = () => {return 'Olá Bundao'}
+// console.log(ola());
+let ola = () => 'Hello Man!'; 
+console.log(ola());
+
+// let jogadores = [
+//     'Piaza',
+//     'Marcelinho',
+//     'Neto',
+//     'Túlio'
+// ];
+
+// for(jog of jogadores){
+//     var arrayNew=[];
+//     arrayNew.push(jog.length);
+//     return arrayNew;
+// }
+// console.log(arrayNew);
+
+// let arrLength = jogadores.map(jogador => jogador.length);
+// console.log(arrLength);
+
+
+
+
+
+
