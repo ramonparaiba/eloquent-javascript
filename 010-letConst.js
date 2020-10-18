8d32a56b018e976bf8cc6c3754fe94ec0a944e87
Pablo Ramon <ramonparaiba@gmail.com>
2020-10-18 21:19:34 +0000

    let const

---
+++ 010-letConst.js
@@ -0,0 +1,17 @@
+//Melhorar este tópico
+
+//undefined
+
+// if(false){
+//     var example = 5;
+// }
+// console.log(example);
+// var example
+// if(true){
+//     example = 5;
+// }
+// console.log(example);
+
+const example ={};
+example.ano = 2020;
+console.log(example);
