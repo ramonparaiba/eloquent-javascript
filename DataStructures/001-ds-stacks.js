ec3dceac912b72aed6d9b1d5d85bb61cd10b0608
ramonparaiba <ramonparaiba@gmail.com>
2020-10-22 04:26:52 -0300

    Aula Pilha

--- DataStructures/001-ds-stacks.js
+++ DataStructures/001-ds-stacks.js
@@ -1,9 +1,26 @@
-let numbs = [2, 3, 5]
+const stack = {
+    "deck" : [],
+    isEmpty(deck){ return (deck.length === 0 ? true: false) },
+    unStack(deck){
+        (isEmpty === true ? console.log("Pilha Vazia"): deck.pop())
+        return deck
+    },
+    stackUp(deck, value){
+        deck.push(value)
+        return deck
+    },
+    top(deck){
+        return deck[deck.length-1]
+    }
+}
 
-numbs.push(6);
 
-console.log(numbs);
-
-numbs.pop();
-
-console.log(numbs);
+let meuDeck = stack.deck
+let isEmpty = stack.isEmpty(meuDeck);
+console.log(stack.isEmpty(meuDeck));
+console.log(stack.unStack(meuDeck));
+console.log(stack.stackUp(meuDeck, 6));
+console.log(stack.stackUp(meuDeck, 15));
+console.log(stack.stackUp(meuDeck, 5));
+console.log(stack.stackUp(meuDeck, 42));
+console.log(stack.top(meuDeck));
